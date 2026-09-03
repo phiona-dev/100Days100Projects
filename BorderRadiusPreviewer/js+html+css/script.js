a1125325d160d2fc90cd5183ec0b105bc259f533
phiona-dev <phionadev@gmail.com>
2026-09-03 11:34:15 +0300

    updated code

--- BorderRadiusPreviewer/js+html+css/script.js
+++ BorderRadiusPreviewer/js+html+css/script.js
@@ -8,6 +8,8 @@ const previewButton = document.getElementById("button-previewer")
 const styling = document.querySelector(".styling")
 const copyBtn = document.getElementById("copy-btn")
 
+let updatedStyle = ""
+
 applyChangesBtn.addEventListener("click", () => {
 
     let topLeftValue = topLeft.value;
@@ -23,21 +25,21 @@ applyChangesBtn.addEventListener("click", () => {
         borderBottomLeftRadius: `${bottomLeftValue || 0}px`
     })
 
-     const updatedStyles = `Object.assign(previewButton.style, {
+     /*const updatedStyles = `Object.assign(previewButton.style, {
         borderTopLeftRadius: "${topLeftValue || 0}px",
         borderTopRightRadius: "${topRightValue || 0}px",
         borderBottomRightRadius: "${bottomRightValue || 0}px",
         borderBottomLeftRadius: "${bottomLeftValue || 0}px"
-    })`
+    })`*/
 
-    styling.textContent = updatedStyles
+    updatedStyles = `border-radius: ${topLeftValue || 0}px ${topRightValue || 0}px ${bottomRightValue || 0}px ${bottomLeftValue || 0}px;`
 
-    copyBtn.addEventListener("click", () => {
+    styling.textContent = updatedStyles    
+})
+
+copyBtn.addEventListener("click", () => {
         navigator.clipboard.writeText(updatedStyles)
             .then(() => alert("Styling copied successfully!"))
             .catch(err => console.error("An error occurred", err))
     })
-
-    
-})
 
